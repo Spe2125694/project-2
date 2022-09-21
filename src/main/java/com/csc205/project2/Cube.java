456e19c285e68a8bece224aeffabcb08a434b64f
Spe2125694 <spe2125694@maricopa.edu>
2022-09-20 18:51:54 -0700

    Initial commit

---
+++ src/main/java/com/csc205/project2/Cube.java
@@ -0,0 +1,27 @@
+package com.csc205.project2;
+
+public class Cube extends Shape {
+
+    private double width;
+
+    public Cube() {
+        super();
+        this.width = 0.0;
+    }
+
+    public Cube(double v) {
+        super();
+        this.width = 0.0;
+
+    }
+
+    @Override
+    public double surfaceArea() {
+        return 0;
+    }
+
+    @Override
+    public double volume() {
+        return 0;
+    }
+}
---
+++ src/main/java/com/csc205/project2/Cylinder.java
@@ -0,0 +1,21 @@
+package com.csc205.project2;
+
+public class Cylinder extends Shape {
+
+    private double height;
+    private double radius;
+
+    public Cylinder(double v, double v1) {
+        super();
+    }
+
+    @Override
+    public double surfaceArea() {
+        return 0;
+    }
+
+    @Override
+    public double volume() {
+        return 0;
+    }
+}
